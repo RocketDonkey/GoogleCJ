11a2f06d6a334bcaa7bcfcf39300055eb5a6ac44
Taylor Bowland <taylor.bowland@gmail.com>
2011-10-30 00:26:38 -0700

    Solved. Works for small and large sets.

--- QRAfrica2010/Reverse Words/Reverse_Words.py
+++ QRAfrica2010/Reverse Words/Reverse_Words.py
@@ -10,12 +10,18 @@ Event: Qualification Round Africa - 2010
 
 Problem A: Store Credit
 
-Given a list of space separated words, reverse the order of the words. Each line of text contains L letters and W words. A line will only consist of letters and space characters. There will be exactly one space character between each pair of consecutive words.
+Given a list of space separated words, reverse the order of the words.
+Each line of text contains L letters and W words.
+A line will only consist of letters and space characters.
+There will be exactly one space character between each pair of consecutive words.
 
 Input
 
 The first line of input gives the number of cases, N.
-N test cases follow. For each test case there will a line of letters and space characters indicating a list of space separated words. Spaces will not appear at the start or end of a line.
+N test cases follow.
+For each test case there will a line of letters and
+space characters indicating a list of space separated words.
+Spaces will not appear at the start or end of a line.
 
 Output
 
@@ -35,3 +41,46 @@ N = 100
 """
 
 import re
+
+#Open input file
+f = open('B-small-practice.in', 'rU')
+f_read = f.readlines()
+
+#Open output file
+o = open('Reverse Words - Small.txt', 'w')
+
+#Add each line to a list
+f_list = []
+for line in f_read:
+    f_list.append(line)
+
+#Determine number of cases
+cases = re.search(r'(\d+)', f_list[0]).group()
+
+#Set main case sentinel
+case = 1
+
+"""PRIMARY LOOP***
+Loop through each list item (i), reversing and outputting
+"""
+for i in range(1, len(f_list)):
+    #Split into a list of words
+    list_words = f_list[i].rsplit(' ')
+
+    #Reverse the list
+    list_reversed = list_words
+    list_reversed.reverse()
+
+    #Remove '\n' from first term (if present)
+    #Interesting - \n appears to be treated as one character
+    list_reversed[0] = list_reversed[0].replace('\n', '')
+    
+    #Determine how many words are in the list
+    list_len = len(list_words)
+
+    #Starting at the endmost value in the original list, add each to list_reversed
+    o.write('Case #' + str(case) + ': ' + (' ').join(list_reversed) + '\n')
+    print 'Case #' + str(case) + ': ' + (' ').join(list_reversed) + '\n'
+
+    #Move to the next case
+    case += 1
