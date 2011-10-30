ae1d7ddec27510dd7a765f76ae97c92125e53d15
Taylor Bowland <taylor.bowland@gmail.com>
2011-10-30 04:58:57 -0700

    Solved. Works for small and large sets.

--- QRAfrica2010/T9 Spelling/T9 Spelling.py
+++ QRAfrica2010/T9 Spelling/T9 Spelling.py
@@ -54,8 +54,70 @@ Large dataset
 import re
 
 def Spelling():
-    pass
+    #Open input file
+    f = open('C-large-practice.in', 'rU')
+    f_read = f.readlines()
 
+    #Open output file
+    o = open('T9 Spelling - Large.txt', 'w')
+
+    #Add file contents to a list
+    list_contents = []
+    for line in f_read:
+        list_contents.append(line)
+
+    #Determine the number of cases
+    cases = re.search(r'(\d+)', list_contents[0]).group()
+
+    #Set up dictionary and add space character
+    num_dict = {}
+    num_dict[chr(32)] = str(0)
+    j = 2
+    i = 97
+    
+    """
+    Char 97 is 'a'
+    Create loop that creates the key dictionary
+    Multiply the string of the value by how much you want to repeat it
+    """
+    while i < 123: #Character after z
+        #If it is a 4-digit key...
+        if i in (112, 119):
+            for k in range(1, 5):
+                num_dict[str(chr(i))] = str(j) * k
+                i += 1
+
+        #3-digit key
+        else:
+            for k in range(1, 4):
+                num_dict[str(chr(i))] = str(j) * k
+                i += 1
+        j += 1
+
+    #Set main loop sentinel and create empty list
+    #Clean this up because it is filthy
+    case = 1
+    letter_map = []
+    final_output = []
+    for p in range(1, len(list_contents)):
+        for letter in list_contents[p]:
+            if letter not in (chr(10)):
+                if letter in num_dict:
+                    letter_map.append(num_dict[letter])
+
+        for i in range(0, len(letter_map)):
+            if i > 0:
+                if letter_map[i][0] == letter_map[i-1][0]:
+                    final_output.append(' ')
+            final_output.append(letter_map[i])
+        print 'Case #' + str(case) + ': ' + ''.join(final_output)
+        o.write('Case #' + str(case) + ': ' + ''.join(final_output) + '\n')
+
+        #Prepare for next cycle
+        case += 1
+        letter_map = []
+        final_output = []
+    
 def main():
     Spelling()
 
